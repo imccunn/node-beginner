69603d3589c582cd6a6b028ed111d574b70ac271
Ian McCunn <ianmccunn@gmail.com>
2015-02-08 19:41:41 -0800

    using exec

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,23 @@
+var exec = require('child_process').exec;
+
 function start() {
   console.log('Request handler \'start\' was called');
+  var content = 'empty';
   
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  });
+
+  /*
   function sleep(milliSeconds) {
     var startTime = new Date().getTime();
     while (new Date().getTime() < startTime + milliSeconds);
   }
+  */
 
-  sleep(10000);
-  return 'Hello Start.';
+  // sleep(10000);
+
+  return content;
 }
 
 function upload() {
