437276f80b6b9ba1ae0ef205467b6c37f1db60a7
Ian McCunn <ianmccunn@gmail.com>
2015-02-08 22:06:59 -0800

    last changes from book instruction

--- requestHandler.js
+++ requestHandler.js
@@ -1,5 +1,6 @@
 var querystring = require("querystring"),
-	fs = require("fs");
+	fs = require("fs"),
+  formidable = require('formidable');
 
 function start(response,postData){
  	console.log("Request handler 'start' was called.");
@@ -22,18 +23,32 @@ function start(response,postData){
 	response.end();
 }
 
-function upload(response,postData){
+function upload(response, request){
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent the text: "+
-	querystring.parse(postData).text);
-	response.end();
+  
+  var form = new formidable.IncomingForm();
+  console.log('about to parse form data');
+  form.parse(request, function(error, fields, files) {
+    console.log('Parsing done');
+
+    fs.rename(files.upload.path, '/tmp/test.png', function (error) {
+      if (error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Received image: <br/>');
+    response.write('<img src=\'/show\' />');
+    response.end();
+  });
+
 }
 
 function show(response){
-	console.log("Request handler 'show' was called.");
-	response.writeHead(200, {"Content-Type": "image/png"});
-	fs.createReadStream("/tmp/test.png").pipe(response);
+	console.log('Request handler \'show\' was called.');
+	response.writeHead(200, {'Content-Type': 'image/png'});
+	fs.createReadStream('/tmp/test.png').pipe(response);
 }
 
 module.exports.start = start;
--- server.js
+++ server.js
@@ -3,21 +3,10 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = '';
     var pathName = url.parse(request.url).pathname;
     console.log('Request for ' + pathName + ' received.'); 
     
-    request.setEncoding('utf8'); // define that we expect the encoding of the recieved data to be UTF-8
-    
-    request.addListener('data', function(postDataChunk) {
-      postData += postDataChunk;
-      console.log('Received POST data chunk \'' + 
-                  postDataChunk + '\'.');
-    });
-    
-    request.addListener('end', function() {
-      route(handle, pathName, response, postData);
-    });
+    route(handle, pathName, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
