31168a04bb4b72e23d7316243c5aa3d2670f0f08
Ian McCunn <ianmccunn@gmail.com>
2015-02-08 21:45:50 -0800

    last fix for request handler

---
+++ requestHandler.js
@@ -0,0 +1,41 @@
+var querystring = require("querystring"),
+	fs = require("fs");
+
+function start(response,postData){
+ 	console.log("Request handler 'start' was called.");
+	
+	var body = '<html>'+
+				'<head>'+
+				'<meta http-equiv="Content-Type" '+
+				'content="text/html; charset=UTF-8" />'+
+				'</head>'+
+				'<body>'+
+				'<form action="/upload" enctype="multipart/form-data" '+
+				'method="post">'+
+				'<input type="file" name="upload">'+
+				'<input type="submit" value="Upload file" />'+
+				'</form>'+
+				'</body>'+
+				'</html>';
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(body);
+	response.end();
+}
+
+function upload(response,postData){
+	console.log("Request handler 'upload' was called.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("You've sent the text: "+
+	querystring.parse(postData).text);
+	response.end();
+}
+
+function show(response){
+	console.log("Request handler 'show' was called.");
+	response.writeHead(200, {"Content-Type": "image/png"});
+	fs.createReadStream("/tmp/test.png").pipe(response);
+}
+
+module.exports.start = start;
+module.exports.upload = upload;
+module.exports.show = show;
